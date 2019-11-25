ddb31c91be3ed7260229dcfe35c284f23060bdc4
Sega Okhiria <sega@dialpad.com>
2019-11-25 15:42:54 -0800

    parse sip body

--- hep/hep.go
+++ hep/hep.go
@@ -234,5 +234,14 @@ func (hepMsg *HepMsg) ParseHep3(udpPacket []byte) error {
 		}
 		currentByte += chunkLength
 	}
+	if len(hepMsg.Body) > 0 {
+        var parseErr error
+        hepMsg.SipMsg, parseErr = parser.ParseMessage(hepMsg.Body, true)
+        if parseErr != nil {
+            return parseErr
+        }
+    } else {
+        return errors.New("Found no body in the HEP3 message")
+    }
 	return nil
 }
